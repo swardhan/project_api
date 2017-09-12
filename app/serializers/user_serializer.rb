fa2ca9fa881f8c862f36c1170118814856dc7349
saumya <saumyawardhan1@gmail.com>
2017-09-13 00:33:44 +0530

    Fixed User serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,24 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :email, :state
+  attributes :id, :full_name, :email, :state, :user_complaints
 
-  has_many :complaints
+  # has_many :complaints
   has_many :comments
+
+  # def user_complaints
+  #   object.complaints.map do |complaint|
+  #     ComplaintSerializer.new(complaint, only: [:id, :title, :description])
+  #   end
+  # end
+
+  def user_complaints
+    object.complaints.collect do |complaint|
+      {
+        id: complaint.id,
+        title: complaint.title,
+        description: complaint.description
+      }
+    end
+  end
 
   # puts object
 
